22cbeed576b48929ad7ecfcd49c8f0ac9817bec8
cengkaya <kayayusuf18@gmail.com>
2022-12-20 07:54:27 +0300

    MvcKampiData1

---
+++ EntityLayer/Concrete/Content.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EntityLayer.Concrete
+{
+    public class Content
+
+    {
+        [Key]
+        public int Contentid { get; set; }
+        [StringLength(1000)]
+        public string ContentValue { get; set; }
+
+        public DateTime ContentDate { get; set; }
+
+        public int Headingid { get; set; }
+        public virtual Heading Heading { get; set; }
+
+        public int? Writerid { get; set; }
+        public Writer Writer { get; set; }
+        
+
+      
+    }
+}
